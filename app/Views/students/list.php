d362931263a07d850f841ec2d4f2040657eb418b
ONE-I0 <davidjayvee22@gmail.com>
2023-05-26 05:06:02 +0800

    finish list and minor adjustments

--- app/Views/students/list.php
+++ app/Views/students/list.php
@@ -25,7 +25,22 @@
     </tr>
   </thead>
   <tbody>
-   
+   <?php foreach($students as $stud): ?>
+    <tr>
+      <th scope="row"><?= $stud['id']; ?></th>
+      <td><?= $stud['student_name']; ?></td>
+      <td><?= $stud['student_number']; ?></td>
+      <td><?= $stud['student_age']; ?></td>
+      <td><?= $stud['student_course']; ?></td>
+      <td><?= $stud['student_level']; ?></td>
+      <td><img src="/Uploads/<?= $stud['student_profile']; ?>"alt="" width="100"></td>
+      <td>
+            <a href=""></a>
+            <a href="/students/edit/<?= $stud['id']; ?>" class="btn btn-primary">Edit</a>
+            <a href="/students/delete/<?= $stud['id']; ?>" class="btn btn-danger">delete</a>
+      </td>
+    </tr>
+    <?php endforeach; ?>
   </tbody>
 </table>
 
