1ae1193b3ae98ceed00a9a1e1d90b98d96b9d29e
ONE-I0 <davidjayvee22@gmail.com>
2023-05-26 05:55:06 +0800

    finish update function and some minor adjustment to codes

--- app/Controllers/StudentsController.php
+++ app/Controllers/StudentsController.php
@@ -43,11 +43,35 @@ class StudentsController extends BaseController
     }
     public function edit_Students($id)
     {
-        
+        $fetchStudent = new StudentsModel();
+        $data['student'] = $fetchStudent->where('id', $id)->first();
+
+        return view('students/edit', $data);
     }
     public function update_Students($id)
     {
-        
+        $updates = new StudentsModel();
+        $db = db_connect();
+        if($img = $this->request->getFile('studentProfile')){
+            if($img->isValid()&& ! $img->hasMoved()){
+                $ProfileName = $img->getRandomName();
+                $img->move('Uploads', $ProfileName);
+            }
+        }
+        if(!empty($_FILES['studentProfile']['name'])){
+            $db->query("UPDATE tbl_students SET student_profile = '$ProfileName' WHERE id = '$id' ");
+        }
+
+        $data = array(
+            'student_name' => $this->request->getPost('studentName'),
+            'student_number' => $this->request->getPost('studentNumber'),
+            'student_age' => $this->request->getPost('studentAge'),
+            'student_course' => $this->request->getPost('studentCourse'),
+            'student_level' => $this->request->getPost('studentLevel'),
+        );
+
+        $updates->update($id, $data);
+        return redirect()->to('/students')->with('success','Successfully Updated');
     }
     public function delete_Students($id)
     {
